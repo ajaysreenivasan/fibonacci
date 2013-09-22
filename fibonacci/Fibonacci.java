36ac24cc1baf90d374da1dbf9f569072e8bf1c13
ajaysreenivasan <ajaysreenivasan@gmail.com>
2013-09-22 00:12:35 -0400

    Fibonacci.java v.1.0

---
+++ fibonacci/Fibonacci.java
@@ -0,0 +1,23 @@
+package fibonacci;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a value: ");
+		int input = sc.nextInt();
+		sc.close();
+		
+		System.out.println(compute(input));
+	}
+	
+	public static int compute(int value){
+		if(value == 1)
+			return 0;
+		else if(value == 2)
+			return 1;
+		else
+			return compute(value - 1) + compute(value - 2);
+	}
+}
